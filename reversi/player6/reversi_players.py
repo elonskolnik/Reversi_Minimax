b276227304fe6f425335137be46f7749f1c12c7f
thydmle <tle3@ithaca.edu>
2019-10-22 20:57:31 -0400

    Fixed __init__ for minimax, the arguments for beam search and all were mistakenly placed in the __init__ for HumanPlayer; fixed line 117 in calling beam_search function, initially had beamSearch(argument), fixed to self.beam_search(argument); added file for transposition table player to test to see if it'll work.

--- reversi/player6/reversi_players.py
+++ reversi/player6/reversi_players.py
@@ -5,7 +5,7 @@ import copy
 
 class HumanPlayer:
 
-    def __init__(self, symbol, beamSearch, killerMove, transposition, qui):
+    def __init__(self, symbol):
         self.symbol = symbol
 
     def get_move(self, board):
@@ -56,10 +56,19 @@ class GreedyPlayer:
 
         return move
 
+
 class MiniMaxPlayer:
     #Minimax agent that calculates 5 levels
-    def __init__(self, symbol):
+    def __init__(self, symbol, beamSearch, killerMove, transposition, qui):
         self.symbol = symbol
+        if beamSearch:
+            self.beamSearch = True
+        if killerMove:
+            self.killerMove = True
+        if transposition:
+            self.transposition = True
+        if qui:
+            self.qui = True
 
     def move_score(self, board):
         score = board.calc_scores().get(self.symbol) - board.calc_scores().get(board.get_opponent_symbol(self.symbol))
@@ -104,8 +113,8 @@ class MiniMaxPlayer:
                 player = "X"
 
         # Get all moves available
-        if(beamSearch):
-            moves = beamSearch(board)
+        if(self.beamSearch):
+            moves = self.beam_search(board)
         else:
             moves = board.calc_valid_moves(player)
         print(moves)
@@ -145,7 +154,8 @@ class MiniMaxPlayer:
             else:
                 return self.find_min_score_in_list(movesdict)
 
-      def beam_search(self, board):
+
+    def beam_search(self, board):
         moves = board.calc_valid_moves(self.symbol)
 
         if len(moves) > 3:
